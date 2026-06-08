721fbd889c6d2d88bacd6d121bb2b47fce3b905d
Caue Taddeo <caue.taddeo@sou.inteli.edu.br>
2026-06-08 11:53:42 -0300

    test: valida gradientes com aproximacao numerica

---
+++ tests/test_gradients.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from mlp import MLP
+
+
+def test_gradient_check_small_network():
+    rng = np.random.default_rng(7)
+    x = rng.normal(size=(5, 4))
+    y = np.array([0, 1, 2, 1, 0])
+    model = MLP([4, 6, 5, 3], seed=3)
+    probs, cache = model.forward(x)
+    assert probs.shape == (5, 3)
+
+    grads = model.backward(cache, y)
+    epsilon = 1e-5
+    max_checks = 20
+    checked = 0
+
+    for param_name, param in model.params.items():
+        for index in np.ndindex(param.shape):
+            original = param[index]
+            param[index] = original + epsilon
+            loss_plus = model.compute_loss(x, y)
+            param[index] = original - epsilon
+            loss_minus = model.compute_loss(x, y)
+            param[index] = original
+
+            numerical = (loss_plus - loss_minus) / (2 * epsilon)
+            analytical = grads[param_name][index]
+            assert np.isclose(numerical, analytical, atol=1e-5)
+
+            checked += 1
+            if checked >= max_checks:
+                return
